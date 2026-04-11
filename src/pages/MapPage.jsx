f504842bfdf2c580dfa37a74378f6b50bd9d1ab5
astauf03 <astauf03@upenn.edu>
2026-04-11 16:06:25 -0400

    setting up the initial dependencies and page creation

---
+++ src/pages/MapPage.jsx
@@ -0,0 +1,10 @@
+function MapPage() {
+    return(
+        <div>
+            <h1>Map Page</h1>
+            <p>This is the map page content.</p>
+        </div>
+    )
+}
+
+export default MapPage
---
+++ src/pages/StoryPage.jsx
@@ -0,0 +1,10 @@
+function StoryPage() {
+    return (
+        <div>
+            <h1>Story Page</h1>
+            <p>This is the story page content.</p>
+        </div>
+    )
+}
+
+export default StoryPage
