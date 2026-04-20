80ce3cd0fff11c09c834f28910b9dc8281a1339e
astauf03 <astauf03@upenn.edu>
2026-04-20 00:56:59 -0400

    mappage css + mappage.jsx updates

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useRef } from 'react'
 import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
+import '../components/mappage.css'
 import NavBar from '../components/NavBar'
 import MapSidebar from '../components/MapSidebar'
-import Footer from '../components/Footer'
+
 
 
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN
@@ -18,13 +19,80 @@ function MapPage() {
 
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v12',  // light basemap for dashboard
+      style: 'mapbox://styles/mapbox/standard',  // light basemap for dashboard
       center: [28.0, -29.0],
       zoom: 5,
       interactive: true,                           // users can pan and zoom
     })
 
     map.current.addControl(new mapboxgl.NavigationControl(), 'bottom-right')
+
+    // Wait for the map to load before adding sources/layers
+    map.current.on('load', () => {
+      console.log('Map loaded!')
+      // You can add any default sources/layers here if needed
+      map.current.addSource('gauteng-wards', {
+        type: 'geojson',
+        data: '/data/gauteng.geojson',
+      })
+      
+      // Fill layer for Gauteng wards
+      map.current.addLayer({
+        id: 'gauteng-fill',
+        type: 'fill',
+        source: 'gauteng-wards',
+        paint: {
+          'fill-color': '#002395',
+          'fill-opacity': 0.5
+        }
+      })
+      
+      // Outline layer for Gauteng wards
+      map.current.addLayer({
+        id: 'gauteng-lines',
+        type: 'line',
+        source: 'gauteng-wards',
+        paint: {
+          'line-color': '#020b25',
+          'line-width': 0.8,
+          'line-opacity': 0.6,
+        },
+      })
+
+
+      // KZN source and layers
+      map.current.addSource('kzn-wards', {
+        type: 'geojson',
+        data: '/data/KZN.geojson',
+      })
+
+      // Fill layer for KZN wards
+      map.current.addLayer({
+        id: 'kzn-wards',
+        type: 'fill',
+        source: 'kzn-wards',
+        paint: {
+          'fill-color': '#ebc159',
+          'fill-opacity': 0.5
+        }
+      })
+
+      // Outline layer for KZN wards
+      map.current.addLayer({
+        id: 'kzn-lines',
+        type: 'line',
+        source: 'kzn-wards',
+        paint: {
+          'line-color': '#4e401a',
+          'line-width': 0.8,
+          'line-opacity': 0.6,
+        },
+      })
+    }) //emd map.on ('load')
+    
+    map.current.on('error', (e) => {
+      console.error('Mapbox error:', e)
+    })
 
     return () => {
       if (map.current) {
@@ -41,8 +109,8 @@ function MapPage() {
         <MapSidebar />
         <div className="map-area" ref={mapContainer} />
       </div>
-      <Footer />
     </div>
+
   )
 }
 
