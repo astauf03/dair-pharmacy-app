a4d87df8c2cdc637022b38c596fca0811b9f516e
astauf03 <astauf03@upenn.edu>
2026-05-01 08:29:34 -0400

    more updates

--- src/pages/StoryPage.jsx
+++ src/pages/StoryPage.jsx
@@ -1,7 +1,6 @@
 import Hero from "../components/Hero";
 import NavBar from "../components/NavBar";
 import ContextBand from "../components/ContextBand";
-import N1 from "../components/N1";
 import N2 from "../components/N2";
 import N3 from "../components/N3";
 import N4 from "../components/N4";
